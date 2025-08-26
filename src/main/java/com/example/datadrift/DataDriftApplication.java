0d5fddea5a5f78b744773116a052126c43146486
Sehar Majid Abdi <seharabdi@Sehars-Laptop.local>
2025-08-26 12:20:20 +0530

    add main application classes

---
+++ src/main/java/com/example/datadrift/DataDriftApplication.java
@@ -0,0 +1,12 @@
+package com.example.datadrift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataDriftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataDriftApplication.class, args);
+    }
+}
